2a898dc112715f6d586321c3006216774e7723c3
Akshay0809 <96840620+Akshay0809@users.noreply.github.com>
2023-03-29 13:13:27 +0530

    Update exception.py

--- src/exception.py
+++ src/exception.py
@@ -30,11 +30,4 @@ class CustomException(Exception):
 
 
 
-if __name__=='__main__':
-
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("divided by zero")
-        raise CustomException(e,sys)
         
